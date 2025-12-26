89459dac37f0f21f601efad5f1723d17809786c2
almuhanovernur4-hue <almuhanovernur4@gmail.com>
2025-12-26 08:38:18 +0500

    Add files via upload

---
+++ vehicle2.java
@@ -0,0 +1,62 @@
+public abstract class Vehicle implements Servicable {
+    protected int id;
+    protected static int idGen = 1;
+    protected String model;
+    protected int year;
+    protected double basePrice;
+
+    public Vehicle(String model, int year, double basePrice) {
+        this.id = idGen++;
+        setModel(model);
+        setYear(year);
+        setBasePrice(basePrice);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null || model.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year < 1900 || year > 2100) {
+            throw new IllegalArgumentException();
+        }
+        this.year = year;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        if (basePrice <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.basePrice = basePrice;
+    }
+
+    public int getAge(int currentYear) {
+        return currentYear - year;
+    }
+
+    public abstract double calculateInsuranceFee();
+
+    @Override
+    public String toString() {
+        return "Vehicle{id=" + id + ", model='" + model + "', year=" + year + ", basePrice=" + basePrice + "}";
+    }
+}
